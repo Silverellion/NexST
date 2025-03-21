25236744aa2151420f8738b25862966bdc6b4a49
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-03-22 04:15:34 +0900

    Added mobile support for contact.html, added better wording for TEMPLATE.html

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,15 +1,15 @@
 document.getElementById("darkModeToggle").addEventListener("click", function () {
     let icon = document.getElementById("modeIcon");
     let panels = document.querySelectorAll(".white-panel");
-    let sidebar = document.querySelector(".sidebar");
+    let sidepanel = document.querySelector(".sidepanel");
 
     if (icon.src.includes("dark-mode.svg")) {
         icon.src = "../icons/nav/light-mode.svg";
         panels.forEach(panel => panel.classList.add("dark-mode"));
-        sidebar.classList.add("dark-mode");
+        sidepanel.classList.add("dark-mode");
     } else {
         icon.src = "../icons/nav/dark-mode.svg";
         panels.forEach(panel => panel.classList.remove("dark-mode"));
-        sidebar.classList.remove("dark-mode");
+        sidepanel.classList.remove("dark-mode");
     }
 });
