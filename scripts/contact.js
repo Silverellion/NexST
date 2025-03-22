66bc84c5c9b7453a07ad080328d1779f320cba10
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-03-22 20:13:01 +0900

    Changed NexST in footer to Next System & Technologies, adjusted the sidepanel of contact in a dark mode to a more unique pallete

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,15 +2,18 @@ document.getElementById("darkModeToggle").addEventListener("click", function () 
     let icon = document.getElementById("modeIcon");
     let panels = document.querySelectorAll(".white-panel");
     let sidepanel = document.querySelector(".sidepanel");
+    let optionalText = document.querySelectorAll(".optional");
 
     if (icon.src.includes("dark-mode.svg")) {
         icon.src = "../icons/nav/light-mode.svg";
         panels.forEach(panel => panel.classList.add("dark-mode"));
         sidepanel.classList.add("dark-mode");
+        optionalText.forEach(el => el.classList.add("dark-mode"));
     } else {
         icon.src = "../icons/nav/dark-mode.svg";
         panels.forEach(panel => panel.classList.remove("dark-mode"));
         sidepanel.classList.remove("dark-mode");
+        optionalText.forEach(el => el.classList.remove("dark-mode"));
     }
 });
 
